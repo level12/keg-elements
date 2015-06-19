6611fd15f4ea06e864eefb65abc92a49dbe6a3b4
Randy Syring <randy@thesyrings.us>
2015-06-19 19:46:55 -0400

    disable CSRF protection for testing

--- kegel_app/config.py
+++ kegel_app/config.py
@@ -1,3 +1,6 @@
 
 class TestProfile(object):
     KEG_KEYRING_ENABLE = False
+    # Turn off CSRF protection for tests.  Makes form testing easier and provides no value during
+    # testing.
+    WTF_CSRF_ENABLED = False
