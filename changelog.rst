f54cb792bf161ba744b8b0e57b2b725a453cb9b9
Randy Syring <randy@thesyrings.us>
2015-07-28 12:11:52 -0400

    add .testing:EntityBase

--- changelog.rst
+++ changelog.rst
@@ -7,3 +7,5 @@ development version: 2015-07-28
 
 * Add db.mixins with DefaultColsMixin (id, Arrow lib UTC timestamps) and MethodsMixin (incomplete).
 * Some MethodsMixin methods now have support for commit/flush parameters.
+* Add .testing:EntityBase which uses named tuples to declare the checks needed and adds some
+  additional logic.
---
+++ keg_elements/testing.py
@@ -0,0 +1,95 @@
+from collections import namedtuple
+
+from keg import current_app
+import sqlalchemy as sa
+from sqlalchemy_utils import ArrowType
+
+from .db.utils import validate_unique_exc
+
+ColumnCheck = namedtuple('ColumnCheck', 'name, required, fk, unique, timestamp')
+ColumnCheck.__new__.__defaults__ = (True, None, None, None)
+
+
+class EntityBase(object):
+    entity_cls = None
+    column_checks = None
+    timestamp_cols = ('created_utc', 'updated_utc')
+    # Set to: 'setup_class', 'setup', or None
+    delete_all_on = 'setup_class'
+
+    @classmethod
+    def setup_class(cls):
+        assert current_app.db_enabled, 'app.db_enabled is False'
+        assert cls.entity_cls, 'The entity_cls attribute must be set.'
+        assert cls.column_checks, 'column_checks should be defined for this entity'
+        if cls.timestamp_cols:
+            if cls.column_checks is None:
+                cls.column_checks = []
+            createdts_col_name, updatedts_col_name = cls.timestamp_cols
+            cls.column_checks.append(ColumnCheck(createdts_col_name, timestamp='create'))
+            cls.column_checks.append(ColumnCheck(updatedts_col_name, timestamp='update'))
+        if cls.delete_all_on:
+            cls.ent_delete_all()
+
+    @classmethod
+    def ent_delete_all(cls):
+        if hasattr(cls.entity_cls, 'delete_cascaded'):
+            cls.entity_cls.delete_cascaded()
+        else:
+            cls.entity_cls.delete_all()
+
+    def setup(self):
+        if self.delete_all_on == 'setup':
+            self.ent_delete_all()
+
+    def test_add(self):
+        self.ent_delete_all()
+        o = self.entity_cls.testing_create()
+        assert self.entity_cls.query.count() == 1
+        if hasattr(self.entity_cls, 'id'):
+            assert o.id
+
+    def check_column_null(self, col, is_required):
+        assert col.nullable != is_required
+
+    def check_column_unique(self, col, is_unique):
+        assert col.unique == is_unique, 'Expected column "{}" to have unique={}'.format(
+            col.name, is_unique)
+
+    def check_column_fk(self, col, fk):
+        fk_count = len(col.foreign_keys)
+        if fk:
+            assert fk_count == 1, 'check_column_fk() can not handle colums w/ multiple FKs'
+            assert fk == list(col.foreign_keys)[0]._get_colspec()
+        else:
+            assert not fk_count, 'Didn\'t expect column "{}" to have a foreign key'.format(col.name)
+
+    def check_column_timestamp(self, col, ts_type):
+        if not isinstance(col.type, (ArrowType, sa.DateTime)):
+            raise AssertionError('Column "{}" is not a recognized timestamp type.'.format(col.name))
+        assert col.default
+        if ts_type == 'update':
+            assert col.onupdate, 'Column "{}" should have onupdate set'.format(col.name)
+        assert col.server_default, 'Column "{}" should have server_default set'.format(col.name)
+
+    def test_column_checks(self):
+        if not self.column_checks:
+            return
+        for col_check in self.column_checks:
+            col = getattr(self.entity_cls, col_check.name)
+            yield self.check_column_null, col, col_check.required
+            yield self.check_column_unique, col, col_check.unique
+            yield self.check_column_fk, col, col_check.fk
+            if col_check.timestamp:
+                yield self.check_column_timestamp, col, col_check.timestamp
+
+    def check_unique_constraint(self, **kwargs):
+        self.entity_cls.testing_create(**kwargs)
+        print(self.entity_cls.query.count())
+        try:
+            self.entity_cls.testing_create(**kwargs)
+            print(self.entity_cls.query.count())
+            raise AssertionError('Uniqueness error was not encountered.')
+        except Exception as e:
+            if not validate_unique_exc(e):
+                raise
