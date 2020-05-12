3a01a93db552bc254db7e1ddc3d00c954d0b6640
Matt Lewellyn <guruofgentoo@gmail.com>
2020-05-12 16:29:58 -0400

    check translations in CI

--- kegel_app/cli.py
+++ kegel_app/cli.py
@@ -5,5 +5,17 @@ def cli_entry():
     KegElApp.cli.main()
 
 
+@KegElApp.cli.command('verify-translations', help='Verifies all strings marked for translation')
+def verify_translations():
+    from pathlib import Path
+    from morphi.messages.validation import check_translations
+
+    root_path = Path(__file__).resolve().parent.parent
+    check_translations(
+        root_path,
+        'keg_elements',
+    )
+
+
 if __name__ == '__main__':
     cli_entry()
